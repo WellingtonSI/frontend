cd11159600794d509f8cf9c9a04a3697305a4952
Wellington Ribeiro <12ribe@gmail.com>
2020-11-26 19:11:28 -0300

    Versão da disgraça

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,65 @@
+import React,{useEffect, useState} from 'react';
+import './style.css'
+import {Link,useHistory,useParams} from "react-router-dom"; 
+import api from '../../services/api'
+
+export default function Profile(){
+    const {id} = useParams();
+    const history = useHistory();
+    const initUser = {
+        name: '',
+        email: '',
+        idade: 0,
+        empresa:''
+    }
+    const [user,setUser] = useState(initUser);
+
+    useEffect(()=>{
+
+            if(id){
+                api.get(`/users/${id}`).then(response=>{
+                    console.log(user)
+                    setUser(...response.data)
+                    
+                })
+            }
+
+        },[])
+
+    function onSubmit(ev){
+        ev.preventDefault();
+        const method = id ? 'put': 'post';
+        const url =id ? `/users/${id}` : '/users' 
+
+        api[method](url,user).then((response)=>{
+            history.push('/')
+        })
+    }
+    function onChange(ev){
+        const {name,value} = ev.target;
+        setUser({...user,[name]:value})
+        console.log(user)
+    }
+    return(
+        <div id="profile-container">
+             <h1>Cadastro</h1>
+             <form onSubmit={onSubmit}>
+                <strong>Nome:</strong>
+                <input name="name" onChange={onChange} value={user.name}/>
+                <strong>Email:</strong>
+                <input type="email" name="email" onChange={onChange} value={user.email}/>
+                <strong>Idade:</strong>
+                <input name="idade" onChange={onChange}  value={user.idade}/>
+                <strong>Empresa:</strong>
+                <input name="empresa" onChange={onChange} value={user.empresa}/>
+                <div className="actions">
+                    <button className="button" type="submit" >Salvar</button>
+                    <Link className="button"  onClick={()=>history.push('/')}>Voltar</Link>
+                </div>
+              
+
+             </form>
+        </div>
+      
+    );
+}
---
+++ src/pages/User/index.js
@@ -0,0 +1,56 @@
+import React,{useState,useEffect} from 'react';
+import api from '../../services/api'
+import {Link} from "react-router-dom"; 
+import './style.css';
+
+
+export default function User(){
+    const [users,setUsers] = useState([]);
+    useEffect(()=> {
+        api.get('users').then(response=>{
+            setUsers(response.data);
+        })
+    },[])
+    async function handleDelete(id){
+        try{
+            await api.delete(`/users/${id}`)
+            setUsers(users.filter(user => user.id != id))
+        }
+        catch(err){
+            alert('Erro ao Deletar!')
+        }
+    }
+    
+    return(
+        
+        <div id="user-container">
+            <h1>Lista de usuário</h1>
+            <Link className="button" id="create-link" to={"/create"} >Criar</Link>
+            <ul className="user-list">
+                {users.map(user =>  (
+                 <li key={user.id}>
+                     <strong>Nome</strong>
+                     <p>{user.name}</p>
+                     <strong>Email</strong>
+                     <p>{user.email}</p>
+                     <strong>idade</strong>
+                     <p>{user.idade}</p>
+                     <strong>Empresa</strong>
+                     <p>{user.empresa}</p>
+
+                     <div className="actions">
+                     <Link className="button" to={`update/${user.id}`} >Acessar</Link>
+                        <button className="button" onClick={()=>handleDelete(user.id)} >Deletar</button>
+                    </div>
+                    
+ 
+                 </li>
+                 
+
+                ))}
+             
+            </ul>
+        </div>
+        
+    );
+}
